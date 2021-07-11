a88e2219cf1640dff5ef84a0874f7f2497a6763d
Zimtir <zimtir@mail.ru>
2021-07-11 03:08:30 +0300

    feat: add simple karatsuba with testing and todo

--- nodejs/src/utils/multiplication.spec.ts
+++ nodejs/src/utils/multiplication.spec.ts
@@ -1,21 +1,39 @@
+import { expect } from 'chai'
 import {
   computeKaratsubaMultiplication,
   computeMultiplication
 } from './multiplication'
-import { expect } from 'chai'
 
 describe('multiplication', () => {
-  it('test simple self-multiplication', () => {
+  it('simple multiplication', () => {
     // Arrange
-    const downBound = -100
-    const upBound = 100
+    const left = 5678
+    const right = 1234
+    const expected = 7006652
+
+    // Actual
+    const defaultActual = computeMultiplication(left, right)
+    const karatsubaActual = Number(computeKaratsubaMultiplication(left, right))
+
+    // Assert
+    expect(expected).equal(karatsubaActual)
+    expect(expected).equal(defaultActual)
+  })
+
+  it('simple self-multiplication', () => {
+    // Arrange
+    const downBound = 20
+    const upBound = 20
 
     for (let index = downBound; index < upBound; index++) {
       const expected = index * index
 
       // Actual
       const defaultActual = computeMultiplication(index, index)
-      const karatsubaActual = computeKaratsubaMultiplication(index, index)
+      const karatsubaActual = computeKaratsubaMultiplication(
+        index.toString(),
+        index.toString()
+      )
 
       // Assert
       expect(expected).equal(defaultActual)
@@ -33,7 +51,10 @@ describe('multiplication', () => {
 
       // Actual
       const defaultActual = computeMultiplication(index, index)
-      const karatsubaActual = computeKaratsubaMultiplication(index, index)
+      const karatsubaActual = computeKaratsubaMultiplication(
+        index.toString(),
+        index.toString()
+      )
 
       // Assert
       expect(expected).equal(defaultActual)
@@ -51,11 +72,36 @@ describe('multiplication', () => {
 
       // Actual
       const defaultActual = computeMultiplication(index, index)
-      const karatsubaActual = computeKaratsubaMultiplication(index, index)
+      const karatsubaActual = computeKaratsubaMultiplication(
+        index.toString(),
+        index.toString()
+      )
 
       // Assert
       expect(expected).equal(defaultActual)
       expect(expected).equal(karatsubaActual)
     }
+  })
+
+  it('zero multiplications', () => {
+    // Arrange
+    const expected = 0
+
+    // Actual
+    const defaultZeroActual = computeMultiplication(0, 0)
+    const defaultLeftNegativeActual = computeMultiplication(-1, 0)
+    const defaultRightNegativeActual = computeMultiplication(0, -1)
+
+    const karatsubaLeftNegativeActual = computeMultiplication(-1, 0)
+    const karatsubaRightNegativeActual = computeMultiplication(0, -1)
+    const karatsubaZeroActual = computeKaratsubaMultiplication('0', '0')
+
+    // Assert
+    expect(expected).equal(defaultZeroActual)
+    expect(expected).equal(defaultLeftNegativeActual)
+    expect(expected).equal(defaultRightNegativeActual)
+    expect(expected).equal(karatsubaZeroActual)
+    expect(expected).equal(karatsubaLeftNegativeActual)
+    expect(expected).equal(karatsubaRightNegativeActual)
   })
 })
